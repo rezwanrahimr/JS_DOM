44b72976924c1bc906ea8377032018bc46ad6bee
rezwanrahimr <rezwanrahim69@gmail.com>
2023-01-12 00:17:42 +0600

    sorting added element

--- hackProgrammingHero/js/main.js
+++ hackProgrammingHero/js/main.js
@@ -1,12 +1,14 @@
-const milestoneData = JSON.parse(data).data;
+let milestoneData = JSON.parse(data).data;
 
 //Load Course Milestone Data
 function loadMilestone() {
   const milestone = document.querySelector(".milestones");
   milestone.innerHTML = `${milestoneData.map((milestone) => {
-    return `<div class="milestone border-b">
-    <div class="flex">
-        <div class="checkbox"><input type="checkbox" /></div>
+    return `<div class="milestone border-b" id='${milestone._id}'>
+    <div class="flex" >
+        <div class="checkbox"><input type="checkbox" onClick="checkList(this,${
+          milestone._id
+        })" /></div>
         <div onClick="openMilestone(this,${milestone._id})">
             <p>
                ${milestone.name}
@@ -43,5 +45,24 @@ function openMilestone(currentMilestone, id) {
 }
 function showMilestone(id) {
   document.querySelector(".milestoneImage").src = milestoneData[id].image;
+  document.querySelector(".milestoneImage").style.opacity = 0;
+  document.querySelector(".title").innerText = milestoneData[id].name;
+  document.querySelector(".details").innerText = milestoneData[id].description;
+}
+document.querySelector(".milestoneImage").onload = function () {
+  this.style.opacity = 1;
+};
+function checkList(check, id) {
+  let milestoneList = document.querySelector(".milestones");
+  let doneList = document.querySelector(".doneList");
+  let items = document.getElementById(id);
+  if (check.checked) {
+    milestoneList.removeChild(items);
+    doneList.appendChild(items);
+  } else {
+    milestoneList.appendChild(items);
+    loadMilestone();
+    doneList.removeChild(items);
+  }
 }
 loadMilestone();
