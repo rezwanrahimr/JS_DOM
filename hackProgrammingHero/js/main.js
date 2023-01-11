b1678f87ea4bc4c60d6f1157783fea79b582647d
rezwanrahimr <rezwanrahim69@gmail.com>
2023-01-11 13:41:25 +0600

    hidden data showing

--- hackProgrammingHero/js/main.js
+++ hackProgrammingHero/js/main.js
@@ -1,2 +1,31 @@
 const milestoneData = JSON.parse(data).data;
-console.log(milestoneData);
+
+//Load Course Milestone Data
+function loadMilestone() {
+  const milestone = document.querySelector(".milestones");
+  milestone.innerHTML = `${milestoneData.map((milestone) => {
+    return `<div class="milestone border-b">
+    <div class="flex">
+        <div class="checkbox"><input type="checkbox" /></div>
+        <div onClick="openMilestone(this)">
+            <p>
+               ${milestone.name}
+                <span><i class="fas fa-chevron-down"></i></span>
+            </p>
+        </div>
+    </div>
+    <div class="hidden_panel">
+        ${milestone.modules.map(function (moduel) {
+          return `<div class="module border-b">
+            <p>${moduel.name}</p>
+        </div>`;
+        })}
+    </div>
+</div>`;
+  })}`;
+}
+function openMilestone(currentMilestone) {
+  const currentPanel = currentMilestone.parentNode.nextElementSibling;
+  currentPanel.classList.toggle("show");
+}
+loadMilestone();
