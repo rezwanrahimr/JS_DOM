a32622724fae04a76b0f3682f22ee0a1b476defd
rezwanrahimr <rezwanrahim69@gmail.com>
2023-01-12 20:06:36 +0600

    done calculation

--- SelectBestFive-pHeroAssignment/main.js
+++ SelectBestFive-pHeroAssignment/main.js
@@ -1,11 +1,24 @@
 function selectFun(id) {
   const playerName = document.getElementById(id).innerText;
   const selectedPlayer = document.querySelector(".player-list");
-  console.dir(selectedPlayer);
-  console.log();
   if (selectedPlayer.children.length <= 4) {
     let testcreate = document.createElement("li");
     testcreate.innerHTML = playerName;
     selectedPlayer.appendChild(testcreate);
   }
+}
+function Calculate() {
+  let perPlayer = document.getElementById("perPlayer").value * 5;
+  document.getElementById("playerExpenses").innerText = perPlayer;
+}
+function calculateTotal() {
+  let playerExpenses = document.getElementById("playerExpenses").innerText;
+  let managerSalary = document.querySelector("#manager").value;
+  let coachSalary = document.querySelector("#Coach").value;
+  let total =
+    Number(playerExpenses) + Number(managerSalary) + Number(coachSalary);
+  console.log(perPlayer);
+  console.log(managerSalary);
+  console.log(coachSalary);
+  document.querySelector("#total").innerText = total;
 }
